c44f61776a5cefca1795ccaed0793db697787cc6
Erlow <erlowgaming@gmail.com>
2024-02-15 21:41:51 +0100

    favicon display taille de la fenetre

--- src/components/fav-icons/fav-icons.tsx
+++ src/components/fav-icons/fav-icons.tsx
@@ -12,10 +12,29 @@ interface FavIconsProps {
 const FavIcons: React.FC<FavIconsProps> = ({ name, url, img, position }) => {
     const handleDrag = (e: any, ui: any) => {
         const { x, y } = ui;
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        const iconWidth = 100; // Taille de l'icône
+        const iconHeight = 100; // Taille de l'icône
+
+        // Vérifier si l'icône sort de l'écran en x
+        if (x < 0) {
+            ui.x = 0;
+        } else if (x + iconWidth > windowWidth) {
+            ui.x = windowWidth - iconWidth;
+        }
+
+        // Vérifier si l'icône sort de l'écran en y
+        if (y < 0) {
+            ui.y = 0;
+        } else if (y + iconHeight > windowHeight) {
+            ui.y = windowHeight - iconHeight;
+        }
+
         const favIcons = JSON.parse(localStorage.getItem('favIcons') || '[]');
         const newFavIcons = favIcons.map((favIcon: any) => {
             if (favIcon.url === url) {
-                favIcon.position = { x, y };
+                favIcon.position = { x: ui.x, y: ui.y };
             }
             return favIcon;
         });
@@ -32,7 +51,7 @@ const FavIcons: React.FC<FavIconsProps> = ({ name, url, img, position }) => {
     }
 
     return (
-        <Draggable grid={[100, 100]} handle=".fav-icons-container" defaultPosition={position} onStop={handleDrag}>
+        <Draggable bounds="parent" handle=".fav-icons-container" defaultPosition={position} onStop={handleDrag} grid={[100, 100]}>
             <div className="fav-icons-container">
                 <span className='delete-icons' onClick={deleteIcon} >&times;</span>
                 <a className='fav-icons-link' target='_blank' rel="noreferrer" href={url}>
